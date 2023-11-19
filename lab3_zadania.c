fd347907af9158424fb92c16aa4d9a3254a6dbaf
Karol Czyż <karol2003czyz@gmail.com>
2023-11-20 00:46:22 +0100

    zadania 1 - 6

---
+++ lab3_zadania.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    
+    //ZAD 1
+    // int tab[10];
+    // int lenght = sizeof(tab) / sizeof(tab[0]);
+    // int x = 3;
+
+    // printf("Elementy tablicy 3,6,9,...: ");
+
+    // for (int i = 0; i < lenght; i++)
+    // {
+    //     tab[i]= x;
+    //     printf("%d ", tab[i]);
+    //     x+=3;
+    // }
+
+    //ZAD 2
+
+    // int tab[10];
+    // int lenght = sizeof(tab) / sizeof(tab[0]);
+    // int x = 3;
+
+    // printf("Elementy tablicy 3,9,15,...: ");
+
+    // for (int i = 0; i < lenght; i+=2)
+    // {
+    //     tab[i]= x;
+    //     printf("%d ", tab[i]);
+    //     x+=3;
+    // }
+
+    //ZAD 3
+
+    // int tab[10];
+    // int lenght = sizeof(tab) / sizeof(tab[0]);
+    // int a = 0,x = 0;
+
+    // printf("Podaj wartosc a: ");
+    // scanf("%d", &a);
+
+    // for (int i = 0; i < lenght; i++)
+    // {
+    //     tab[i]= a - x;
+    //     printf("%d ", tab[i]);
+    //     x+=5;
+    // }
+    
+    //ZAD 4
+    
+    // int tab[10];
+    // int lenght = sizeof(tab) / sizeof(tab[0]);
+    // int losowaLiczba, min = tab[0];
+
+    // srand(time(NULL));
+
+    // for (int i = 0; i < lenght; i++)
+    // {
+    //     tab[i]= losowaLiczba;
+    //     printf("%d ", tab[i]);
+    //     losowaLiczba= rand() % 50 -23 ;
+
+    //     if(tab[i] < min)
+    //     {
+    //         min = tab[i];
+    //     } 
+    // }
+    
+    // printf("%d", min);
+    
+    //ZAD 5
+
+    // int tab[10];
+    // int lenght = sizeof(tab) / sizeof(tab[0]);
+
+    // scanf("%d %d", &tab[0], &tab[1]);
+
+    // for (int i = 2; i < lenght; i++)
+    // {
+    //     tab[i] = tab[i - 2] + tab[i - 1];
+    // }
+
+    // for (int i = 0; i < lenght; i++)
+    // {
+    //     printf("%d ", tab[i]);
+    // }
+
+    //ZAD 6
+
+    // int tab[20] = {1,2,3,4,5,6,7,8,9,10};
+    // int lenght = sizeof(tab) / sizeof(tab[0]);
+
+    // for (int i = 10; i < lenght; i++)
+    // {
+    //     tab[i] = tab[i - 10];
+    // }
+
+    // for (int i = 0; i < lenght; i++)
+    // {
+    //     printf("%d ", tab[i]);
+    // }
+    
+
+
+    return 0;
+
+}
